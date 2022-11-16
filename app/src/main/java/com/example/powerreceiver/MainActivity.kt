7042c2b2f06c39cfd76ab2c23bb1b7df8bcbe1fb
wlsdn9297 <wlsdn9297@naver.com>
2022-11-16 11:28:59 +0900

    인텐트 작업으로 인텐트 필터 만들기

--- app/src/main/java/com/example/powerreceiver/MainActivity.kt
+++ app/src/main/java/com/example/powerreceiver/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.powerreceiver
 
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.powerreceiver.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    private val mReceiver = CustomReceiver()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val filter = IntentFilter()
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED)
+        filter.addAction(Intent.ACTION_POWER_CONNECTED)
     }
 }
