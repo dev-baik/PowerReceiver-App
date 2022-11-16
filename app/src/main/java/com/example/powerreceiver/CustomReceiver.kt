775aa541dc94942b9c847d14273bb26aa19b834f
wlsdn9297 <wlsdn9297@naver.com>
2022-11-16 21:47:51 +0900

    Homework 추가 데이터

--- app/src/main/java/com/example/powerreceiver/CustomReceiver.kt
+++ app/src/main/java/com/example/powerreceiver/CustomReceiver.kt
@@ -10,14 +10,16 @@ class CustomReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         // 이 메서드는 BroadcastReceiver가 인텐트 브로드캐스트를 수신할 때 호출됩니다.
         val intentAction = intent.action
+        val stringNumber = intent.getStringExtra(MainActivity.EXTRA_RANDOM_NUMBER)
+        val number = stringNumber!!.toInt()
 
         val toastMessage = when (intentAction) {
             Intent.ACTION_POWER_CONNECTED -> "Power connected!"
             Intent.ACTION_POWER_DISCONNECTED -> "Power disconnected"
-            ACTION_CUSTOM_BROADCAST -> "Custom Broadcast Received"
+            MainActivity.ACTION_CUSTOM_BROADCAST -> "Custom Broadcast Received"
             else -> "unknown intent action"
         }
 
-        Toast.makeText(context, toastMessage, Toast.LENGTH_SHORT).show()
+        Toast.makeText(context, "$toastMessage\nSquare of the Random number: " + number * number, Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/powerreceiver/MainActivity.kt
+++ app/src/main/java/com/example/powerreceiver/MainActivity.kt
@@ -2,16 +2,20 @@ package com.example.powerreceiver
 
 import android.content.Intent
 import android.content.IntentFilter
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.example.powerreceiver.databinding.ActivityMainBinding
-
-const val ACTION_CUSTOM_BROADCAST = BuildConfig.APPLICATION_ID + ".ACTION_CUSTOM_BROADCAST"
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    companion object {
+        const val ACTION_CUSTOM_BROADCAST = BuildConfig.APPLICATION_ID + ".ACTION_CUSTOM_BROADCAST"
+        const val EXTRA_RANDOM_NUMBER = "extra_random_number"
+    }
 
     private val mReceiver = CustomReceiver()
 
@@ -40,6 +44,16 @@ class MainActivity : AppCompatActivity() {
 
     fun sendCustomBroadcast(view: View) {
         val customBroadcastIntent = Intent(ACTION_CUSTOM_BROADCAST)
-        LocalBroadcastManager.getInstance(this).sendBroadcast(customBroadcastIntent)
+
+        val num = randomNum().toString()
+        binding.number.text = num
+
+        customBroadcastIntent.putExtra(EXTRA_RANDOM_NUMBER, num)
+        LocalBroadcastManager.getInstance(this)
+            .sendBroadcast(customBroadcastIntent)
+    }
+
+    private fun randomNum(): Int {
+        return Random().nextInt(20) + 1
     }
 }
